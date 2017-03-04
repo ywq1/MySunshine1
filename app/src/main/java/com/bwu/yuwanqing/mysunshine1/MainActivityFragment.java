66883d1c8251064986e20e6a0cef9cc645cd6f3e
ywq <1454072136@qq.com>
2017-03-04 16:43:32 +0800

    create array adaptor

--- app/src/main/java/com/bwu/yuwanqing/mysunshine1/MainActivityFragment.java
+++ app/src/main/java/com/bwu/yuwanqing/mysunshine1/MainActivityFragment.java
@@ -5,6 +5,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -14,6 +15,7 @@ import java.util.List;
  * Created by yuwanqing on 2017-03-04.
  */
 public class MainActivityFragment extends Fragment {
+    ArrayAdapter<String> mForecastAdapter;
     public MainActivityFragment() {
     }
     @Override
@@ -29,6 +31,15 @@ public class MainActivityFragment extends Fragment {
                 "Sun 2/17 - Sunny - 20/7"
         };
         List<String> weekForecast = new ArrayList<String>(Arrays.asList(data));
+        //Now that we have some dummy forecast data, create an ArrayAdapter.
+        //The ArrayAdapter will take data from a source (like ore dummy forecast) and
+        //use it to populate the ListView it's attached to.
+        mForecastAdapter =
+                new ArrayAdapter<String>(
+                        getActivity(),
+                        R.layout.list_item_forecast,
+                        R.id.list_item_forecast_textview,
+                        weekForecast);
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
         return rootView;
     }
