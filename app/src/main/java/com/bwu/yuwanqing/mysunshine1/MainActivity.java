13e1024f2b40576ae9c00b27da2550c9bc986758
ywq <1454072136@qq.com>
2017-03-08 16:15:10 +0800

    run 5,设置setting菜单，但是应该在Details界面出现的数据没出现。

--- app/src/main/java/com/bwu/yuwanqing/mysunshine1/MainActivity.java
+++ app/src/main/java/com/bwu/yuwanqing/mysunshine1/MainActivity.java
@@ -1,5 +1,6 @@
 package com.bwu.yuwanqing.mysunshine1;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -40,6 +41,7 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            startActivity(new Intent(this, SettingsActivity.class));
             return true;
         }
 
