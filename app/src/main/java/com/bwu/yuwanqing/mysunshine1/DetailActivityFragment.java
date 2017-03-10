d820afcd8f16ffeae5113ff4e61abc9a23205ab1
ywq <1454072136@qq.com>
2017-03-10 11:56:30 +0800

    run 11, share via message

--- app/src/main/java/com/bwu/yuwanqing/mysunshine1/DetailActivityFragment.java
+++ app/src/main/java/com/bwu/yuwanqing/mysunshine1/DetailActivityFragment.java
@@ -3,11 +3,16 @@ package com.bwu.yuwanqing.mysunshine1;
 import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.support.v7.widget.ShareActionProvider;
 
 import org.w3c.dom.Text;
 
@@ -15,22 +20,53 @@ import org.w3c.dom.Text;
  * A placeholder fragment containing a simple view.
  */
 public class DetailActivityFragment extends Fragment {
-    private final String LOG_TAG1 = DetailActivityFragment.class.getSimpleName();
+    private static final String LOG_TAG = DetailActivityFragment.class.getSimpleName();
+    private static final String FORECAST_SHARE_HASHTAG = " #SunshineApp";
+    private String mForecastStr;
 
     public DetailActivityFragment() {
+        setHasOptionsMenu(true);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
+        Log.v("DetailActivityFragment:","onCreateView");
         //The detail Activity called via intent. Inspect the intent for forecast data.
         Intent intent = getActivity().getIntent();
         if(intent != null && intent.hasExtra(Intent.EXTRA_TEXT)) {
-            String forecastStr = intent.getStringExtra(Intent.EXTRA_TEXT);
+            mForecastStr = intent.getStringExtra(Intent.EXTRA_TEXT);
             TextView textView = (TextView) rootView.findViewById(R.id.detail_text);
-            textView.setText(forecastStr);
+            textView.setText(mForecastStr);
         }
         return rootView;
+    }
+    @Override
+    public void onCreateOptionsMenu (Menu menu, MenuInflater inflater) {
+        // Inflate the menu; this adds items to the action bar if it is present;
+        inflater.inflate(R.menu.detailfragment, menu);
+
+        // Retrieve找回，检索 the share menu item定位使用sharedActionProvider的菜单项
+        MenuItem menuItem = menu.findItem(R.id.action_share);
+
+        // Get the provider and hold onto it to set/change the share intent.
+        ShareActionProvider mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
+
+        // Attach an intent to this SharedProvider. You can update this at any time,
+        // like when the user selects a new piece of data they might like to share.
+        if(mShareActionProvider != null) {
+            mShareActionProvider.setShareIntent(createShareForecastIntent());
+        }
+        else {
+            Log.d(LOG_TAG, "Share Action Provider is null");
+        }
+    }
+    private  Intent createShareForecastIntent() {
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+        shareIntent.setType("text/plain");
+        shareIntent.putExtra(Intent.EXTRA_TEXT, mForecastStr + FORECAST_SHARE_HASHTAG);
+        return shareIntent;
     }
 }
