2956f3eca07dd0a817cb856967d58e0139de1ce5
ywq <1454072136@qq.com>
2017-03-10 12:14:05 +0800

    run 12, life cycle

--- app/src/main/java/com/bwu/yuwanqing/mysunshine1/MainActivity.java
+++ app/src/main/java/com/bwu/yuwanqing/mysunshine1/MainActivity.java
@@ -19,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.v(LOG_TAG,"onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -27,9 +28,40 @@ public class MainActivity extends AppCompatActivity {
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.fragment, new ForecastFragment())
                     .commit();
-            Log.v("MainActivity:","onCreate");
         }
     }
+    @Override
+    protected void onStart() {
+        Log.v(LOG_TAG, "in onStart");
+        super.onStart();
+        //The activity is about to become visible.
+    }
+    @Override
+    protected void onResume() {
+        Log.v(LOG_TAG, "in onResume");
+        super.onResume();
+        // The activity has become visible (it is now "resumed")
+    }
+    @Override
+    protected void onPause() {
+        Log.v(LOG_TAG, "in onParse");
+        super.onPause();
+        // Another activity is taking focus (this activity is about to be "paused")
+    }
+    @Override
+    protected void onStop() {
+        Log.v(LOG_TAG, "in onStop");
+        super.onStop();
+        // The activity is no longer visible (it is now "stopped")
+    }
+    @Override
+    protected void onDestroy() {
+        Log.v(LOG_TAG, "in onDestroy");
+        super.onDestroy();
+        // The activity is about to be destroyed.
+    }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
